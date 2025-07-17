4dd7efb79308c8fb0d7bdb1396fdbb8278b772f8
Mst.Kanij Fatima <123645381+BMKanij@users.noreply.github.com>
2025-07-18 00:28:55 +0600

    Update (task 2).php

--- performance 5/(task 2).php
+++ performance 5/(task 2).php
@@ -1,1 +1,35 @@
+<?php
+class Book {
+    public $title;
+    public $available;
 
+    public function __construct($title, $available) {
+        $this->title = $title;
+        $this->available = $available;
+    }
+
+    public function isAvailable() {
+        if ($this->available) {
+            return "Available";
+        } else {
+            return "Not Available";
+        }
+    }
+}
+
+
+$book1 = new Book("Ami Topu", true);
+$book2 = new Book("Harry Potter", false);
+$book3 = new Book("Ema", true);
+$book4 = new Book ("Ilabrito",false);
+$book5 = new Book ("Rusha",false);
+
+
+$books = [$book1, $book2, $book3, $book4, $book5];
+
+foreach ($books as $book) {
+    echo $book->title . " is " . $book->isAvailable() . "<br>";
+}
+
+
+?>
