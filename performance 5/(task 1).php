5d3c924c29ee5157a0d1cdf8ddc634a6985e069b
Mst.Kanij Fatima <123645381+BMKanij@users.noreply.github.com>
2025-07-18 00:24:21 +0600

    Update (task 1).php

--- performance 5/(task 1).php
+++ performance 5/(task 1).php
@@ -1,1 +1,32 @@
+<?php
+$string = "this is a demo lab.";
+$vowels = ['a', 'e', 'i', 'o', 'u'];
+$found_vowels = [];
+$result = "";
 
+
+for ($i = 0; $i < strlen($string); $i++) {
+    $char = strtolower($string[$i]);
+    if (in_array($char, $vowels)) {
+        $found_vowels[] = $char;
+    }
+}
+
+echo "Vowels found: " . implode(", ", $found_vowels) . "<br>";
+
+
+for ($i = 0; $i < strlen($string); $i++) {
+    $char = $string[$i];
+    if (in_array(strtolower($char), $vowels)) {
+        $result .= 'l';  
+    } else {
+        $result .= $char;  
+    }
+}
+
+
+echo $result;
+
+
+
+?>
